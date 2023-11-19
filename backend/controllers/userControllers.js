41861f0426fceb09c2544e5363d0004c12fe2258
Ayush Joshi <ayushjoshitnp@gmail.com>
2023-11-20 00:17:28 +0530

    added search user api

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,13 +3,37 @@ const User = require('../models/userModel');
 
 const getProfile = asyncHandler(async (req, res, next) => {
     console.log('getProfile is called');
+
     const userEmail = req.headers.loggedInUserEmail;
     const user = await User.findOne({ email : userEmail }, { password: 0 });
-
     if(!user) {
         res.status(400).json('User not found.');
     }
     res.status(200).json(user);
 })
 
-module.exports = { getProfile }
+const searchUser = asyncHandler(async (req, res, next) => {
+    console.log('searchUser is called');
+
+    const searchKey = req.query.searchKey;
+    let pageNo = req.query.pageNo;
+    let pageSize = req.query.pageSize;
+    if(!pageNo) {
+        pageNo = 1;
+    }
+    if(!pageSize) {
+        pageSize = 10;
+    }
+    const users = await User.find({ 
+        $or: [
+            {'email': { $regex: '^' + searchKey, $options:'i' }}, 
+            {'name': { $regex: '^' + searchKey, $options:'i' }}
+        ]}, 
+        { password:0 })
+        .skip((pageNo-1)*pageSize)
+        .limit(pageSize);
+        
+    res.status(200).json(users);
+})
+
+module.exports = { getProfile, searchUser }
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getProfile } = require('../controllers/userControllers');
+const { getProfile, searchUser } = require('../controllers/userControllers');
 
-router.get('/getProfile', getProfile)
+router.get('/getProfile', getProfile);
+
+router.get('/search', searchUser);
 
 module.exports = router;
