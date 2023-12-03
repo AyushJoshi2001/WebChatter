293e35de69db4317c670701878136ee17cc0461c
Ayush Joshi <ayushjoshitnp@gmail.com>
2023-12-03 22:53:14 +0530

    updated project structure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/auth-container/login/login.component';
 import { RegisterComponent } from './components/auth-container/register/register.component';
 import { HomeComponent } from './components/app-container/home/home.component';
 
-export const routes: Routes = [
+const routes: Routes = [
   {path: '', redirectTo: 'app', pathMatch: 'full'},
   {
     path: 'auth',
@@ -21,3 +22,9 @@ export const routes: Routes = [
     ]
   }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
